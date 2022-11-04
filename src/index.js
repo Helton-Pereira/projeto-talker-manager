4ccaef6875d93212ccaad6d624df376a370a9297
Helton Pereira <helton.bfsm@gmail.com>
2022-11-04 15:18:36 -0300

    Req 1 (aparentemente) cumprido

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
 
+app.use(express.json());
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+const talkersPath = path.resolve(__dirname, './talker.json');
+
+const getTalkers = async () => {
+  try {
+    const data = await fs.readFile(path.resolve(talkersPath));
+    const response = await JSON.parse(data);
+    return response;
+  } catch (error) {
+    console.error(`Arquivo não pôde ser lido: ${error}`);
+  }
+};
 
 // não remova esse endpoint. É para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  try {
+    const talkers = await getTalkers();
+    res.status(200).json(talkers);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 app.listen(PORT, () => {
